cd5cd8612e4e5191cc74af77faf73aba9f981146
Oskar Truffer <oskar.truffer@gmail.com>
2014-10-02 18:05:14 +0200

    dusted out the second sql update step

--- sql/dbupdate.php
+++ sql/dbupdate.php
@@ -5,10 +5,3 @@
     $conf = $pl->getConfigObject();
     $conf->initDB();
 ?>
-<#2>
-<?php
-require_once('./Customizing/global/plugins/Services/Cron/CronHook/Dustman/classes/class.ilDustmanPlugin.php');
-$pl = new ilDustmanPlugin();
-$conf = $pl->getConfigObject();
-$conf->initDB();
-?>

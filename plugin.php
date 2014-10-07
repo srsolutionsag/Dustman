e3798ee78056bd020ebbe752af929bf008c33dfa
Fabian Schmid <fs@studer-raimann.ch>
2014-10-07 14:46:01 +0200

    Only ILIAS 4.4 supported

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 $id = "dustman";
 $version = "1.0.0";
 $ilias_min_version = "4.4.0";
-$ilias_max_version = "4.5.999";
+$ilias_max_version = "4.4.999";
 $responsible = "Oskar Truffer";
 $responsible_mail = "ot@studer-raimann.ch";
 ?>

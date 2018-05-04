15b4acae5631ea77f38a920f3cad0121524c2f96
Benjamin Seglias <bs@studer-raimann.ch>
2018-05-04 11:44:50 +0200

    increased plugin version, ilias min and max version

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id = "dustman";
-$version = "1.1.0";
-$ilias_min_version = "5.1.0";
-$ilias_max_version = "5.2.999";
+$version = "1.2.0";
+$ilias_min_version = "5.2.0";
+$ilias_max_version = "5.3.999";
 $responsible = "studer + raimann ag";
 $responsible_mail = "info@studer-raimann.ch";

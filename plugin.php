47b17e15de615bf69a9a00aa14266ba51a025b0a
Robin Baumgartner <rb@studer-raimann.ch>
2019-08-09 09:41:49 +0200

    Increase maximum supported ilias version

--- plugin.php
+++ plugin.php
@@ -2,6 +2,6 @@
 $id = "dustman";
 $version = "1.2.0";
 $ilias_min_version = "5.2.0";
-$ilias_max_version = "5.3.999";
+$ilias_max_version = "5.4.999";
 $responsible = "studer + raimann ag";
 $responsible_mail = "info@studer-raimann.ch";

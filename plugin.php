f7a803753e023cbe3677c867d4ecd776d5aab9e6
Fabian Schmid <fs@studer-raimann.ch>
2020-10-06 10:11:09 +0200

    plugin version

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id                = 'dustman';
-$version           = '1.3.0';
-$ilias_min_version = '5.2.0';
+$version           = '1.3.1';
+$ilias_min_version = '5.3.0';
 $ilias_max_version = '5.4.999';
 $responsible       = 'studer + raimann ag';
-$responsible_mail  = 'info@studer-raimann.ch';
+$responsible_mail  = 'support@studer-raimann.ch';
